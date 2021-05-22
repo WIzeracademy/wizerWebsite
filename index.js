86ce376d5c3df8164c063e7cc7893aadcbe4fd4a
Ashutosh Sharma <ashutoshsharma9865@gmai9l.com>
2021-05-23 05:20:24 +0530

    authentication Completed

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
- const express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
- const app = express();
- app.use(express.static('public'));
- app.get("/",(req,res)=>{
-     res.sendFile("./index.html");
- });
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/wizerDB",
+    { useNewUrlParser: true,useUnifiedTopology: true }
+);
+
+
+require("./models/User");
+require("./services/passport.js");
+
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
  app.listen(PORT);
